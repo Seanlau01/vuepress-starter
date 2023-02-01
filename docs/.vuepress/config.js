2c2c6cc9dd0177966c65ff6c77d63696ecbf14f8
Seanlau <lausean561@gmail.com>
2023-02-01 20:05:09 +0800

    upload repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,14 @@
 module.exports={
+  base:'/vuepress-starter/',
+  title:'Louis blogs',
+  description:'this is my built blog sys',
+  head: [
+    ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
+    ['meta',{ name:'keywords',content:'vuepress 新上手'}],
+    ['meta', { author:'louis',content:'louis blogs'}]
+  ],
     themeConfig: {
         logo: '/assets/img/hero.png',
-       
-    
-        
         nav: [
             { text: 'Home', link: '/' },
             { text: 'External', link: 'https://google.com', target:'_self', rel:'' },
